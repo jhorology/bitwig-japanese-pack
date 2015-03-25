5556be0818fd1b64610b3d017b335793c37e3a9d
jhorology <jhorology2014@gmail.com>
2015-03-25 22:27:37 +0900

    update diff.sh

--- diff.sh
+++ diff.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # diff between current and last versions
 #
-diff -c /Users/masafumi/Documents/jhorology/bitwig-japanese-pack/target/test/resources/Bitwig\ Studio-resources.properties /Applications/Bitwig\ Studio.app/Contents/Resources/localization/Bitwig\ Studio-resources.properties
 
-diff -c /Users/masafumi/Documents/jhorology/bitwig-japanese-pack/target/test/resources/GUIApplication-resources.properties /Applications/Bitwig\ Studio.app/Contents/Resources/localization/GUIApplication-resources.properties
+cd `dirname $0`
+
+diff -c target/test/resources/Bitwig\ Studio-resources.properties /Applications/Bitwig\ Studio.app/Contents/Resources/localization/Bitwig\ Studio-resources.properties
+
+diff -c target/test/resources/GUIApplication-resources.properties /Applications/Bitwig\ Studio.app/Contents/Resources/localization/GUIApplication-resources.properties
