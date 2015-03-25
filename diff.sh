761680bb43b31c56a3d6d9e00eba7191b232898b
jhorology <jhorology2014@gmail.com>
2015-03-25 22:22:14 +0900

    support 1.1.7RC2

---
+++ diff.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# diff between current and last versions
+#
+diff -c /Users/masafumi/Documents/jhorology/bitwig-japanese-pack/target/test/resources/Bitwig\ Studio-resources.properties /Applications/Bitwig\ Studio.app/Contents/Resources/localization/Bitwig\ Studio-resources.properties
+
+diff -c /Users/masafumi/Documents/jhorology/bitwig-japanese-pack/target/test/resources/GUIApplication-resources.properties /Applications/Bitwig\ Studio.app/Contents/Resources/localization/GUIApplication-resources.properties
